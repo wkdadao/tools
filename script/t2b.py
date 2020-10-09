b9d87a6411fff14901c7a3a35e17a1dae8c4a5e7
rainexus <rainexus@gmail.com>
2020-10-09 11:02:50 +0800

    update

--- script/t2b.py
+++ script/t2b.py
@@ -1,11 +1,22 @@
 import binascii
 import sys
+import os
 
-tFile = sys.argv[1]
-bFile = tFile + '.bin'
+tFolder = sys.argv[1]
+bFile = tFolder + '.bin'
 
-with open(tFile, 'r') as tf:
-    readStr = tf.read()    
-    readBytes = binascii.a2b_base64(readStr)
-    with open(bFile, 'wb') as bf:
-        bf.write(readBytes)
+allReadStr = ''
+fileIndex = 1
+while True:
+    tFile = tFolder + '/' + str(fileIndex) + '.txt'
+    if not os.path.exists(tFile):
+        break
+    print(tFile)
+    with open(tFile, 'r') as tf:
+        readStr = tf.read()
+        allReadStr += readStr
+    fileIndex += 1
+        
+readBytes = binascii.a2b_base64(allReadStr)
+with open(bFile, 'wb') as bf:
+    bf.write(readBytes)
