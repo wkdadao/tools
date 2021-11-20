5199c36519714d67cc9af3765a7ec3c060f6ba3a
rainexus <rainexus@gmail.com>
2021-11-20 13:16:15 +0800

    programming

---
+++ Programming/Java/Java动态代理机制详解（JDK 和CGLIB，Javassist，ASM） - 我的程序人生 - 博客频道 - CSDN.NET_files/ad-blog.js
@@ -0,0 +1,35 @@
+﻿var ad_type = 'ms1149';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms1141_2';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms1143_3';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ ad_type = 'ms1151_4';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+/*
+ ad_type = 'ms846_5';
+ ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+*/
